5f30efb311181eb5156f1b1ff2aefcb2262c29e2
Peres_05 <isaacperes05@gmail.com>
2024-09-11 15:31:00 -0300

    2024.09.11

--- js/template.js
+++ js/template.js
@@ -1,21 +1,47 @@
 function runApp() {
-    $('#wrap>nav').hide();
-    $('#fumaca').hide();
-    $('#toggleMenu').click(controlaMenu)
+
+    /* Oculta o menu na inicialização */
+    resizeMenu();
+
+    /* Quando a janela for redimensionada chama resizeMenu() */
+    $(window).resize(resizeMenu);
+
+    /* Quando do botão do menu for clicado chama a função toggleMenu() */
+    $('#toggleMenu').click(toggleMenu);
 }
 
-function controlaMenu() {
+
+function resizeMenu() {
+    if (window.innerWidth >= 640) {
+        showMenu();
+    } else {
+        hideMenu();
+    }
+}
+
+function toggleMenu() {
 
     if ($('#wrap>nav').is(':visible')) {
-        $('#wrap>nav').hide('fast');
-        $('#fucama')
+        hideMenu();
     } else {
-        $('#wrap>nav').show('fast');
+        showMenu();
     }
 
     return false;
 }
 
+/* Exibe o menu */
+function showMenu() {
+    $('#wrap>nav').show('fast');
+    $('#menuSmoke').show('fast');
+    $('body').css('overflow', 'hidden');
+}
 
+/* oculta o menu */
+function hideMenu() {
+    $('#wrap>nav').hide('fast');
+    $('#menuSmoke').hide('fast');
+    $('body').css('overflow', 'auto');
+}
 
 $(document).ready(runApp);
